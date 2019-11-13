12d89b6bb0419620f5aa32bac980d24520372e4c
JOHNSON JOSEPH <johnsonjoseph@JOHNSONs-MacBook-Air.local>
2019-11-13 10:03:37 -0800

    added Navbar to navbar.js and add styles to the navbar

--- alumni-client/src/components/Navbar.js
+++ alumni-client/src/components/Navbar.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+    li {
+        list-style: none; 
+    }
+
+    ul {
+        display: flex;
+        justify-content: space-around;
+    }
+`;
+
+//list-style removes the bullets 
 
 
-const Navbar = () => <nav>
-    <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/alumni/new">Create Alumni</Link></li>
-        <li><Link to="/alumni">My Alumni</Link></li>
-
-    </ul>
-</nav>
+const Navbar = () => (
+    <StyledNav>
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/alumni/new">Create Alumni</Link></li>
+                <li><Link to="/alumni">My Alumni</Link></li>
+            </ul>
+        </nav>
+    </StyledNav>
+);
 
 export default Navbar
